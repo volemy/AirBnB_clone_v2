cb1f7dd039e65db74f538c7f42cc8cecaa65cd96
Brokeice123 <keithmarkomen@gmail.com>
2024-01-16 23:37:32 +0000

    Trying to fix error

--- models/state.py
+++ models/state.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+"""Defines the State class."""
+import models
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
+    """
+    State class
+    """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
 
-    #For DBStorage
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
-
-    #For FileStorage
-    @property
-    def cities(self):
-        """Returns the list of City instances with state_id"""
-        from models import storage
-        new_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                new_list.append(city)
-        return new_list
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
