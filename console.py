037c032a96af0e6e37aad000f6def0e685dd9f4d
Brokeice123 <keithmarkomen@gmail.com>
2024-01-17 08:35:06 +0000

    Console fixing

--- console.py
+++ console.py
@@ -153,6 +153,7 @@ class HBNBCommand(cmd.Cmd):
 
         storage.save()
         print(new_instance.id)
+        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
