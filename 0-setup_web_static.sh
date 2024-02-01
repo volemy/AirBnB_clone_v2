0747437c73abfd0f5c5dda522b4476925c3bf83f
volemy <minayobenevolence0@gmail.com>
2024-02-01 11:12:08 +0000

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 # Script that sets up your web servers for the deployment of web_static.
-#!/bin/bash
-
-# Install Nginx if it not already installed
 if ! command -v nginx &> /dev/null
 then
     sudo apt-get update
