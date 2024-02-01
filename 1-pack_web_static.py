b0930795f90609557ed9b96028bccf5fe440941e
volemy <minayobenevolence0@gmail.com>
2024-02-01 11:27:52 +0000

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This module contains a script that generates a .tgz archive from the contents
-of the web_static folder of your AirBnB Clone repo
+This module contains a script that generates a .tgz archive from  contents
+of the web_static folder of  AirBnB Clone repo
 """
 import os.path
 from fabric.api import local
@@ -9,7 +9,7 @@ from datetime import datetime
 
 
 def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder"""
+    """Generates a .tgz archive from contents of the web_static folder"""
     local("mkdir -p versions")
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = "versions/web_static_{}.tgz".format(date)
