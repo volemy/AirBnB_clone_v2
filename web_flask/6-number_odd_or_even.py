aacabe3ff90a5d927591b5e96b1d2b83c48af85b
volemy <minayobenevolence0@gmail.com>
2024-02-17 20:17:59 +0000

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -52,7 +52,7 @@ def number_template(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even_route(n):
-"""This method displays a HTML page only if n is an integer:
+    """This method displays a HTML page only if n is an integer:
     H1 tag: “Number: n is even|odd” inside the tag BODY
     """
     return render_template('6-number_odd_or_even.html', n=n)
