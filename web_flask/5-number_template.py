716024899b6a89a3d31ae68a2a3d3b93a8ae7b7f
volemy <minayobenevolence0@gmail.com>
2024-02-17 19:55:59 +0000

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -5,7 +5,8 @@
 """
 
 
-from flask import Flask, request, render_template
+from flask import Flask, request,
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -48,7 +49,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """This returns an HTML page only if n is an int"""
-    return number_template('5-number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
